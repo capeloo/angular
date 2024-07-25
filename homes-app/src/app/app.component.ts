821d040b458fe3183adb04da2cc5db35dcd76b58
Caio Henrique Capêlo <caiocapelo@alu.ufc.br>
2024-07-25 09:54:36 -0300

    up

--- homes-app/src/app/app.component.ts
+++ homes-app/src/app/app.component.ts
@@ -1,14 +1,11 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `
-    <h1>Default</h1>
-  `,
+  selector: 'app-root',
+  template: `<h1>Hello World!</h1>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'default';
+  title = 'homes';
 }
--- homes-app/src/main.ts
+++ homes-app/src/main.ts
@@ -1,10 +1,5 @@
-/*
- *  Protractor support is deprecated in Angular.
- *  Protractor is used in this example for compatibility with Angular documentation tools.
- */
-import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
-import {AppComponent} from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {providers: [provideProtractorTestingSupport()]}).catch((err) =>
-  console.error(err),
-);
+bootstrapApplication(AppComponent)
+  .catch(err => console.error(err));
